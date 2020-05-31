9d6c7e265ec1e4d4b56d5ee43319c1fd4144647a
Vedavathi GH <vedavathi.gh@gmail.com>
2020-06-01 01:18:08 +0530

    added may test

--- routes/normalRoutes/blogNormalRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var {renderCreateBlog,renderUpdateBlog} = require('../../controllers/normalControllers/blogNormalControllers');
-var authenticate = require('../../middlewares/authenticate');
-
-router.get('/blog/create', authenticate, renderCreateBlog);
-router.get('/blog/update/:blogId', authenticate, renderUpdateBlog);
-
-module.exports = router;
